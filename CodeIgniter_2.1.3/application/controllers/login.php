de3263c52b910ff4a473599052ff7be3a1c55f48
alexDev <alexdev@vBox.(none)>
2013-02-16 19:15:10 -0700

    fixed loading views from login controller function'

--- CodeIgniter_2.1.3/application/controllers/login.php
+++ CodeIgniter_2.1.3/application/controllers/login.php
@@ -13,13 +13,13 @@ class Login extends CI_Controller {
 		if ($data['username'] === 'admin')
 		{
 			$this->load->view('templates/header', $data);
-			$this->load->view('admin_homepage', $data);
+			$this->load->view('pages/admin_homepage', $data);
 			$this->load->view('templates/footer', $data);
 		}
 		else
 		{
 			$this->load->view('templates/header', $data);
-			$this->load->view('homepage', $data);
+			$this->load->view('pages/homepage', $data);
 			$this->load->view('templates/footer', $data);
 		}
 	}
