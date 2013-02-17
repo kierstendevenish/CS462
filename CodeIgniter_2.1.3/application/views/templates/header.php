8071d886e544dd1768a83fd4e96152f13bb15ef2
alexDev <alexdev@vBox.(none)>
2013-02-16 17:21:48 -0700

    updated header and footer

--- CodeIgniter_2.1.3/application/views/templates/header.php
+++ CodeIgniter_2.1.3/application/views/templates/header.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+	<title><?php echo $title ?> - CS462 Lab 1</title>
 </head>
 <body>
-	<h1>CodeIgniter 2 Tutorial</h1>
+	<h1>CS462 Lab 1</h1>
