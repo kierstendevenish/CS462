2b98a60e1447a521be9bf2f1ac84cd484c88a51c
alexDev <alexdev@vBox.(none)>
2013-02-18 10:55:45 -0700

    trying to display json contents as strings

--- CodeIgniter_2.1.3/application/views/pages/homepage.php
+++ CodeIgniter_2.1.3/application/views/pages/homepage.php
@@ -1,4 +1,13 @@
 This is <?php echo $username; ?>'s homepage.
 <br>
 <a href='pages/account'>My Account</a><br><br>
-Json: <?php echo $json; ?>
+Json: 
+<?php 	
+	foreach ($json as $user)
+	{
+		foreach ($user as $elem)
+		{
+			echo $elem + '<br>';
+		}
+	} 
+?>
