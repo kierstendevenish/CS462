09f6bc1a2443f644d499b4207c618b7221812376
alexDev <alexdev@vBox.(none)>
2013-02-18 16:34:28 -0700

    adding user model to correct model folder

---
+++ CodeIgniter_2.1.3/application/models/userModel.php
@@ -0,0 +1,35 @@
+class UserModel extends CI_Model {
+	
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+
+	function checkExists($username = "", $json = "")
+	{
+		foreach ($json['users'] as $user)
+		{
+			if ($username == $user['username'])
+			{
+				return true;
+			}
+		}
+
+		return false;	
+	}
+
+	function addUser($username = "", $password = "", $json = "")
+	{
+		if ($username != "" && $username != "")
+		{
+			$id = $this->getNewId($json);
+			array_push($json['users'], array("id" -> $id, "username" = $username, "password" = $password));
+		}
+	}
+
+	function getNewId($json = "")
+	{
+		
+	}
+}
