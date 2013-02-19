421d89303c41708a03d1ded3e4d95c344ad11bc9
alexDev <alexdev@vBox.(none)>
2013-02-18 18:23:17 -0700

    homepage

--- CodeIgniter_2.1.3/application/views/pages/homepage.php
+++ CodeIgniter_2.1.3/application/views/pages/homepage.php
@@ -1,6 +1,4 @@
-This is <?php echo $username; ?>'s homepage.
-<br>
-<a href='pages/account'>My Account (<?php echo $user['username']; ?>)</a><br><br>
+My Account (<?php echo $user['username']; ?>)<br><br>
 
 ESL: <?php if (array_key_exists('esl', $user)) echo $user['esl']; ?> <a href="">Edit</a>
 
