6acd80f7d17c7023a1f362bf4d7153f890a0f618
alexDev <alexdev@vBox.(none)>
2013-02-20 00:19:23 -0700

    trying to fix session

--- CodeIgniter_2.1.3/application/controllers/home.php
+++ CodeIgniter_2.1.3/application/controllers/home.php
@@ -1,6 +1,6 @@
 
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-session_start(); //we need to call PHP's session object to access it through CI
+//session_start(); //we need to call PHP's session object to access it through CI
 class Home extends CI_Controller {
 
  function __construct()
