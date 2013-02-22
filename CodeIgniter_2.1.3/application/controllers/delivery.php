885ccadf6fb5e423404f9b11dd8ad889b4baf22c
alexDev <alexdev@vBox.(none)>
2013-02-21 20:15:26 -0700

    fix delivery controller

--- CodeIgniter_2.1.3/application/controllers/delivery.php
+++ CodeIgniter_2.1.3/application/controllers/delivery.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Register extends CI_Controller {
+class Delivery extends CI_Controller {
 
 	function __construct()
  	{
