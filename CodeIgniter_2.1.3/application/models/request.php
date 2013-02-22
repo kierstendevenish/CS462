f15936c482cfe3bdd04eb884f361d05a3de52e71
alexDev <alexdev@vBox.(none)>
2013-02-21 21:57:05 -0700

    saving new requests as delivered=false

--- CodeIgniter_2.1.3/application/models/request.php
+++ CodeIgniter_2.1.3/application/models/request.php
@@ -5,7 +5,7 @@ Class Request extends CI_Model
         function create($shopAddr, $pickupTime, $deliveryAddr, $deliveryTime)
         {
                 $db = new PDO('sqlite:./application/db/flowershop');
-                $result = $db->query("INSERT INTO Requests (shopAddr, pickupTime, deliveryAddr, deliveryTime) VALUES ('" . $shopAddr . "','" . $pickupTime . "','" . $deliveryAddr . "','" . $deliveryTime . "');");
+                $result = $db->query("INSERT INTO Requests (shopAddr, pickupTime, deliveryAddr, deliveryTime, delivered) VALUES ('" . $shopAddr . "','" . $pickupTime . "','" . $deliveryAddr . "','" . $deliveryTime . "',false);");
         }
         
         function allOpen()
