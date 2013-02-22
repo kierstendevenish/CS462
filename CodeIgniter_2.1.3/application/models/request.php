4dff0687ce2ad79af18aed9bf8bdec1df37bc9b3
alexDev <alexdev@vBox.(none)>
2013-02-21 23:25:44 -0700

    change false to 0

--- CodeIgniter_2.1.3/application/models/request.php
+++ CodeIgniter_2.1.3/application/models/request.php
@@ -5,7 +5,7 @@ Class Request extends CI_Model
         function create($shopAddr, $pickupTime, $deliveryAddr, $deliveryTime)
         {
                 $db = new PDO('sqlite:./application/db/flowershop');
-                $result = $db->query("INSERT INTO Requests (shopAddr, pickupTime, deliveryAddr, deliveryTime, delivered) VALUES ('" . $shopAddr . "','" . $pickupTime . "','" . $deliveryAddr . "','" . $deliveryTime . "',false);");
+                $result = $db->query("INSERT INTO Requests (shopAddr, pickupTime, deliveryAddr, deliveryTime, delivered) VALUES ('" . $shopAddr . "','" . $pickupTime . "','" . $deliveryAddr . "','" . $deliveryTime . "',0);");
         }
         
         function allOpen()
