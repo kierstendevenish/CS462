0e567981bb6c939ad9fb32dd238b40e107db17c6
alexDev <alexdev@vBox.(none)>
2013-02-23 22:44:34 -0700

    trying css

--- CodeIgniter_2.1.3/application/views/templates/header.php
+++ CodeIgniter_2.1.3/application/views/templates/header.php
@@ -1,7 +1,8 @@
 <html>
 <head>
 	<title>Flower Shop</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."/application/css/style.css"; ?>">
+        <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()."/application/css/style.css"; ?>"> -->
+        <style> @import url('<?=base_url()?>/application/css/styles.css'); </style>
 </head>
 <body>
 	<h1>Flower Shop</h1>
