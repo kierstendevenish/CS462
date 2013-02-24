03602c3c851b748cdc6d6f2097aaf5b7944bd1c0
alexDev <alexdev@vBox.(none)>
2013-02-23 22:54:08 -0700

    config for dev

--- CodeIgniter_2.1.3/application/views/templates/header.php
+++ CodeIgniter_2.1.3/application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>Flower Shop</title>
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."/application/css/style.css"; ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()."application/css/style.css"; ?>">
         <!-- <style> @import url('<?=base_url()?>/application/css/styles.css'); </style> -->
 </head>
 <body>
