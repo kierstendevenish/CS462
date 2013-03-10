392309b034426b59f76449622293e4fc4623a0cc
alexDev <alexdev@vBox.(none)>
2013-03-09 19:49:24 -0700

    first commit...copy of flower shop website

--- CodeIgniter_2.1.3/application/views/templates/header.php
+++ CodeIgniter_2.1.3/application/views/templates/header.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Flower Shop</title>
+	<title>Driver Portal</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url()."application/css/style.css"; ?>">
         <!-- <style> @import url('<?=base_url()?>/application/css/styles.css'); </style> -->
 </head>
@@ -11,5 +11,5 @@
         </ul>
     </div>
     <div id="body">
-        <title>Flower Shop</title>
-        <br><h1>Flower Shop</h1><br>
+        <title>Driver Portal</title>
+        <br><h1>Driver Portal</h1><br>
