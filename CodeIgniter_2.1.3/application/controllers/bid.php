6046672b839ac3f6873c56fe5a7e34587c101d61
alexDev <alexdev@vBox.(none)>
2013-03-18 21:13:17 -0600

    debug

--- CodeIgniter_2.1.3/application/controllers/bid.php
+++ CodeIgniter_2.1.3/application/controllers/bid.php
@@ -9,12 +9,15 @@ class Bid extends CI_Controller {
 
 	function consume()
         {
-            var_dump("here");
             $this->load->model('request');
             $deliveryId = $this->input->post('deliveryId');
             $driverName = $this->input->post('driverName');
             $estDeliveryTime = $this->input->post('estDeliveryTime');
             $rate = $this->input->post('rate');
+            var_dump($deliveryId);
+            var_dump($driverName);
+            var_dump($estDeliveryTime);
+            var_dump($rate);
 
             $this->request->saveBid($deliveryId, $driverName, $estDeliveryTime, $rate);
         }
